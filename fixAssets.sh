f3cb622aba2021aae97c6194ff1c8526db15170b
Ryno Mathee <rmathee@gmail.com>
2016-12-21 07:47:24 +0200

    Fix markdown.js references

--- fixAssets.sh
+++ fixAssets.sh
@@ -14,6 +14,7 @@ assets/plugins/framework/framework.js
 assets/plugins/boostrapv3/js/bootstrap.js
 assets/plugins/boostrapv3/css/bootstrap.css
 assets/plugins/jquery-scrollbar/jquery.scrollbar.js
+assets/plugins/markdown/markdown.js
 assets/css/font-awesome-buttons.css
 assets/css/framework-icons.css
 assets/css/framework.css
